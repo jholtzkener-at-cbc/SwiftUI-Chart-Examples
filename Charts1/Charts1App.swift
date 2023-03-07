d404e03055146e368336ba04fccb4102b3e0fabe
Jeff Holtzkener <jeffrey.steven.holtzkener@cbc.ca>
2023-03-07 15:42:03 -0500

    Initial Commit

---
+++ Charts1/Charts1App.swift
@@ -0,0 +1,17 @@
+//
+//  Charts1App.swift
+//  Charts1
+//
+//  Created by cbcmusic on 2023-03-07.
+//
+
+import SwiftUI
+
+@main
+struct Charts1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
